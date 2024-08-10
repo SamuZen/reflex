88a2a267148a9ddacfb2f0ffdc50132ec9bf1f80
Samu <Pedroqueiroz.zen@gmail.com>
2024-08-10 17:17:51 -0300

    adjust promise require

--- src/Promise.lua
+++ src/Promise.lua
@@ -3,10 +3,10 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local packages = script:FindFirstAncestor("rbxts_include")
 	or script:FindFirstAncestor("include")
 	or ReplicatedStorage:FindFirstChild("rbxts_include")
-	or script.Parent.Parent
+	or script.Parent.Parent.Packages
 
-if packages and packages:FindFirstChild("Promise") then
-	return require(packages.Promise)
+if packages and packages:FindFirstChild("promise") then
+	return require(packages.promise)
 else
 	error(`Could not find Promise from {script:GetFullName()}`)
 end
